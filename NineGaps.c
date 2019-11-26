5c37d9c530fd043f62ed0cbcf1bf919483de9469
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2019-11-25 23:13:56 -0500

    Update NineGaps.c

--- NineGaps.c
+++ NineGaps.c
@@ -1,9 +1,3 @@
-*/
-    Harnoor Singh Reen 
-    110006294
-*/
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -38,17 +32,14 @@ int calculate(int a, char x, int b, char y, int c)
         else if (y == '*')
             result = a * b * c;
     }
-
     else
     {
         printf("Something went wrong");
     }
     return result;
 }
-
 int main()
 {
-
     int main_board[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // It defines the main board to store all the values from 1-9
     int missing_array[9];                            // This line generates an array of size 9 to keep track of missing values
     int game_board[9];                               // It defines an array to form the actual game board with random numbers and hidden values
@@ -62,9 +53,7 @@ int main()
     char check_game;                                 // this is a flag variable to determine if the user solves the game correctly
     int play_again;                                  // this is a flag variable to determine if the user wants to play again or not
     int missing_count;                               // this is a variable that keep track of the number of missing values in the game board.
-
     srand(time(NULL)); // It generates a seed, using current time, for the random generator
-
     do
     {
         check_game = 1;
@@ -74,7 +63,6 @@ int main()
         printf("      *****************      \n");
         printf("            *****            \n");
         printf("              *              \n");
-
         // get the level of difficulty from the user
         do
         {
@@ -103,9 +91,7 @@ int main()
             }
         } while (1);
         //////////////////////////////////////////////////
-
         //Generation of a random main board by shuffling the main_board
-
         for (i = 0; i < 9; i++)
         {
             int temp = main_board[i];
@@ -115,9 +101,7 @@ int main()
             main_board[randomIndex] = temp;
         }
         /////////////////////////////////////////////////
-
         //Generation of random operators ( + , - , x ) with number 1,2,3 representing addition, subtraction and multiplication respectively
-
         int num;
         for (i = 0; i < 12; i++)
         {
@@ -129,18 +113,14 @@ int main()
             else if (num == 3)
                 operators_array[i] = '*';
         }
-
         /////////////////////////////////////////////////
-
         //Calculate the results in columns and rows, the creation of the game board
-
         row_results[0] = calculate(main_board[0], operators_array[0], main_board[1], operators_array[1], main_board[2]);
         row_results[1] = calculate(main_board[3], operators_array[2], main_board[4], operators_array[3], main_board[5]);
         row_results[2] = calculate(main_board[6], operators_array[4], main_board[7], operators_array[5], main_board[8]);
         column_results[0] = calculate(main_board[0], operators_array[6], main_board[3], operators_array[9], main_board[6]);
         column_results[1] = calculate(main_board[1], operators_array[7], main_board[4], operators_array[10], main_board[7]);
         column_results[2] = calculate(main_board[2], operators_array[8], main_board[5], operators_array[11], main_board[8]);
-
         ///////////////////////////////////////////////////////
         //Initializing missing array to 0
         for (i = 0; i < 9; i++)
@@ -149,18 +129,13 @@ int main()
         }
         ////////////////////////////////////////////////////////
         //Copying the main_board into game_board
-
         for (i = 0; i < 9; i++)
         {
             game_board[i] = main_board[i];
         }
-
         /////////////////////////////////////////////////////////
-
         // Based on the selected level, 3 or 5 or 7 board values will be hidden.
-
         i = 0; // initializing i to 0
-
         while (i < level * 2 + 1)
         {
             int r = rand() % 3;
@@ -172,11 +147,8 @@ int main()
                 i++;
             } // end of if
         }     // end of while
-
         ////////////////////////////////////////////////////////
-
         //Copy the game board into a temporary board
-
         int temp_board[9];
         for (i = 0; i < 9; i++)
         {
@@ -220,18 +192,14 @@ int main()
             }
             for (i = 0; i < 3; i++) // show the columnar results
                 printf(" %d\t\t", column_results[i]);
-            printf("\n---------------------------------------------------\n");
-
+            printf("\n---------------------------------------------------\n");\
             ////////////////////////////////////////////////////////
-
             //Displaying the missing values and updating the missing_count variable      (COMPLETE THIS PART)
-
             missing_count = 0;
             for (i = 0; i < 9; i++)
             {
                 if (missing_array[i] != 0)
                 {
-
                     printf("%d\t", missing_array[i]);
                     missing_count++;
                 }
@@ -243,7 +211,6 @@ int main()
             {
                 break;
             }
-
             /////////////////////////////////////////////////////
             // Get user's guess (location and value) and check inputs' validity
             printf("Enter a row number (1-3), column(1-3), value(One of the missing values):\n");
@@ -251,25 +218,21 @@ int main()
             scanf("%d %d %d", &r, &c, &v);
             if (r == 0 || c == 0 || v == 0)
                 break;
-
             if (r < 1 || r > 3 || c < 1 || c > 3)
             {
                 puts("Invalid row and/or column numbers. Try again.");
                 continue;
             }
-
             if (v < 1 || v > 9)
             {
                 puts("Invalid cell value. Try again.");
                 continue;
             }
-
             if (missing_array[v - 1] < 1)
             {
                 puts("This value is already there. Try again.");
                 continue;
             }
-
             // If the selected cell is changeable, add the value into the cell and remove it from the missing values, in other case show an error and repeat the inner loop.
             //Also checking the values
             int ch = 0;
@@ -335,21 +298,16 @@ int main()
             }
             if (missing_count == 0)
                 return 1;
-
             //////////////////////////////////////////////////////
-
         } while (1); // end of the inner loop
-
         if (check_game) // display Winning/Losing Message
             printf("**** Congratulations!!!****\n ***You Won!!!!*** \n");
         else
             printf(":( Sorry it is not correct :( \n");
-
         printf("#######################################\n");
         printf("   Do you want to play again? (Yes:1, No:0)\n"); // ask the user to play again
         printf("#######################################\n");
         scanf("%d", &play_again);
-
         if (!play_again)
         {
             printf("Bye!");
