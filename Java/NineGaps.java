613e64944d52213812f1a44412c82531c2e238f1
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2021-01-03 03:43:51 +0530

    Update NineGaps.java

--- Java/NineGaps.java
+++ Java/NineGaps.java
@@ -73,9 +73,12 @@ public class NineGaps {
                         case 4:
                             System.out.println("Expert Level");
                             break;
+                        default:
+                            break;
                     }
-                    break;
+                    // break;
                 }
+                break;
             } while (true);
             //Generation of a random main board by shuffling the mainBoard
             for (i = 0; i < 9; i++) {
@@ -105,10 +108,37 @@ public class NineGaps {
             columnResults[0] = calculate(mainBoard[0], operatorsArray[6], mainBoard[3], operatorsArray[9], mainBoard[6]);
             columnResults[1] = calculate(mainBoard[1], operatorsArray[7], mainBoard[4], operatorsArray[10], mainBoard[7]);
             columnResults[2] = calculate(mainBoard[2], operatorsArray[8], mainBoard[5], operatorsArray[11], mainBoard[8]);
-
+            //Copying the mainBoard into gameBoard
             for (i = 0; i < 9; i++) {
                 gameBoard[i] = mainBoard[i];
             }
+            // Based on the selected level, 3 or 5 or 7 board values will be hidden.
+            i = 0;
+            while (i < level * 2 + 1) {
+                Random rand = new Random();
+                int r = rand.nextInt(3);
+                int c = rand.nextInt(3);
+                if (gameBoard[3 * r + c] != '?') {
+                    missingArray[gameBoard[3 * r + c] - 1] = gameBoard[3 * r + c];
+                    gameBoard[3 * r + c] = '?';
+                    i++;
+                }
+            }
+            //Copying game board to a temporary board
+            for (i = 0; i < 9; i++) {
+                tempBoard[i] = gameBoard[i];
+            }
+            do {
+                System.out.println("Game Board:");
+                System.out.println("---------------------------------------------------");
+                for (i = 0; i < 3; i++) {
+                    if (i > 0) {
+                        for (j = 0; j < 3; j++) {
+                            //System.out.println(" %c\t\t", operatorsArray[3 * (i + 1) + j]);
+                        }
+                    }
+                }
+            } while (true);
         } while (true);
     }
 }
