e34382686ab11ff03e878259970d6e460ae6439a
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2021-01-03 17:07:32 +0530

    Update NineGaps.java

--- Java/NineGaps.java
+++ Java/NineGaps.java
@@ -42,7 +42,7 @@ public class NineGaps {
         int i, j;
         int randNum, temp1;
         int level;
-        int checkGame;
+        char checkGame;
         int playAgain;
         int missingCount;
         do {
@@ -128,29 +128,127 @@ public class NineGaps {
             for (i = 0; i < 9; i++) {
                 tempBoard[i] = gameBoard[i];
             }
+            //The inner loop to get values from the user
             do {
-                System.out.println("Game Board:");
-                System.out.println("---------------------------------------------------");
+                // Display the game board after each update
+                // Display the game board after each update
+                System.out.println("Game Board:\n");
+                System.out.println("---------------------------------------------------\n");
                 for (i = 0; i < 3; i++) {
-                    if (i > 0) {
+                    if (i > 0) { // show the column opertors
                         for (j = 0; j < 3; j++) {
                             System.out.printf(" %c\t\t", operatorsArray[3 * (i + 1) + j]);
                         }
+                        System.out.println();
                     }
-                    for (j = 0; j < 3; j++) {
+                    for (j = 0; j < 3; j++) { // show the rows
                         if (gameBoard[(i * 3) + j] != '?')
                             System.out.printf(" %d\t ", tempBoard[(i * 3) + j]);
-                        else if (gameBoard[(i * 3) + j] != tempBoard[(i * 3) + j])
+                        else if (gameBoard[(i * 3) + j] != tempBoard[(i * 3) + j]) {
                             System.out.printf(" %d?\t ", tempBoard[(i * 3) + j]);
-                        else
-                            System.out.printf(" ?\t ");
+                        } else
+                            System.out.print(" ?\t ");
                         if (j < 2)
                             System.out.printf(" %c\t", operatorsArray[(i * 2) + j]);
                         else
-                            System.out.printf("= %d", rowResults[i]);
+                            System.out.printf("= %d", rowResults[i]); // show the rows results
+                    }
+                    System.out.println();
+                    if (i == 2)
+                        System.out.println(" =\t\t =\t\t =\n");
+                }
+                for (i = 0; i < 3; i++) // show the columnar results
+                    System.out.printf(" %d\t\t", columnResults[i]);
+                System.out.println("\n---------------------------------------------------\n");
+                ////////////////////////////////////////////////////////
+                //Displaying the missing values and updating the missing_count variable      (COMPLETE THIS PART)
+                missingCount = 0;
+                for (i = 0; i < 9; i++) {
+                    if (missingArray[i] != 0) {
+                        System.out.printf("%d\t", missingArray[i]);
+                        missingCount++;
                     }
                 }
+                System.out.println();
+                /////////////////////////////////////////////////////////
+                //Break if the missing values are empty      (COMPLETE THIS PART)
+                if (missingCount == 0) {
+                    break;
+                }
+                /////////////////////////////////////////////////////
+                // Get user's guess (location and value) and check inputs' validity
+                System.out.println("Enter a row number (1-3), column(1-3), value(One of the missing values):\n");
+                Scanner scan = new Scanner(System.in);
+                int r, v, c = scan.nextInt();
+                if (r == 0 || c == 0 || v == 0)
+                    break;
+                if (r < 1 || r > 3 || c < 1 || c > 3) {
+                    System.out.println("Invalid row and/or column numbers. Try again.");
+                    continue;
+                }
+                if (v < 1 || v > 9) {
+                    System.out.println("Invalid cell value. Try again.");
+                    continue;
+                }
+                if (missingArray[v - 1] < 1) {
+                    System.out.println("This value is already there. Try again.");
+                    continue;
+                }
+                // If the selected cell is changeable, add the value into the cell and remove it from the missing values, in other case show an error and repeat the inner loop.
+                //Also checking the values
+                int ch = 0;
+                switch (r) {
+                    case 1:
+                        for (i = 0; i < 3; i++) {
+                            if (mainBoard[i] == v) {
+                                if (c == i + 1) {
+                                    tempBoard[i] = v;
+                                    ch = 1;
+                                }
+                            }
+                        }
+                        if (ch == 0)
+                            System.out.println("\nIncorrect. Re-enter value\n");
+                        break;
+                    case 2:
+                        for (i = 3; i < 6; i++) {
+                            if (mainBoard[i] == v) {
+                                if (c == i - 2) {
+                                    tempBoard[i] = v;
+                                    ch = 1;
+                                }
+                            }
+                        }
+                        if (ch == 0)
+                            System.out.println("\nIncorrect. Re-enter value\n");
+                        break;
+                    case 3:
+                        for (i = 6; i < 9; i++) {
+                            if (mainBoard[i] == v) {
+                                if (c == i - 5) {
+                                    tempBoard[i] = v;
+                                    ch = 1;
+                                }
+                            }
+                        }
+                        if (ch == 0)
+                            System.out.println("\nIncorrect. Re-enter value\n");
+                        break;
+                    default:
+                        System.out.println("\nIncorrect. Re-enter value\n");
+                }
+                if (ch == 1) {
+                    if (missingArray[v - 1] >= 1) {
+                        missingArray[v - 1] = 0;
+                    } else
+                        System.out.println("\nPlease try again\n");
+                    continue;
+                }
+                if (missingCount == 0)
+                    return 1;
             } while (true);
+            if (checkGame)
+                System.out.println("You Won!");
         } while (true);
     }
 }
